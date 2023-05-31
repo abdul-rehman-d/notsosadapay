22401ca1b854f3deaf52c321f150421f702bec00
Abdul Rehman <ar618211@gmail.com>
2023-05-31 13:27:28 +0500

    new colors

--- colors.js
+++ colors.js
@@ -4,11 +4,12 @@ module.exports = {
 	primaryLight: '#FFF2F1',
 	secondary: '#0DCCAE',
 	secondaryMedium: '#01E3BD',
+	secondaryDark: '#005244',
 	secondaryLight: '#E6FAF8',
 	accent: '#1D9DE6',
 	accentMedium: '#1FACFD',
 	foreground: '#F6F6F5',
 	transBlack: 'rgba(0,0,0,0.1)',
 	background: '#F2F6F7',
-	black: '#151515'
+	black: '#151515',
 }
